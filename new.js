614f4da00621389ce7e2ba83cfc7049319c839ed
dulshandadallege <xcrt@gmail.com>
2024-07-30 18:00:09 +0530

    2nd commit

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
 var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// print if the number is even and odd
+// print if the number is Even and odd
 nums.forEach(function (num) {
     if (num % 2 == 0) {
         console.log(num + " is even");
